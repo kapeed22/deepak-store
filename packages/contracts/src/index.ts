0bd415354752831116b96a7064c854c5eaea396b
DEEPAK <91515394+kapeed22@users.noreply.github.com>
2026-09-08 15:52:08 +0530

    Update index.ts

--- packages/contracts/src/index.ts
+++ packages/contracts/src/index.ts
@@ -2,3 +2,7 @@ export * from "./order";
 export * from "./payment";
 export * from "./inventory";
 export * from "./notification";
+
+export * from "./messaging/payment";
+export * from "./messaging/order";
+export * from "./messaging/notification";
