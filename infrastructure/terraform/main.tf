8b77828f97baec62f3ea5b88fa9fcea852e0b771
DEEPAK <91515394+kapeed22@users.noreply.github.com>
2026-09-23 15:37:15 +0530

    Create main.tf

---
+++ infrastructure/terraform/main.tf
@@ -0,0 +1,11 @@
+# Deepak Store infrastructure will be added incrementally.
+#
+# Planned resources:
+# - VPC
+# - Subnets
+# - Security groups
+# - RDS PostgreSQL
+# - ElastiCache
+# - SQS
+# - ECR
+# - EKS
