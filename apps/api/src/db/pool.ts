ed2e0060411471ce345ac27b3e1a3116948c7226
DEEPAK <91515394+kapeed22@users.noreply.github.com>
2026-09-23 14:44:17 +0530

    Create pool.ts

---
+++ apps/api/src/db/pool.ts
@@ -0,0 +1,9 @@
+import { Pool } from "pg";
+import { env } from "../config/env";
+
+export const pool = new Pool({
+  connectionString: env.databaseUrl,
+  max: 10,
+  idleTimeoutMillis: 30_000,
+  connectionTimeoutMillis: 5_000
+});
