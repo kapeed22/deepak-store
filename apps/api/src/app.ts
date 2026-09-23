19b8227b3087f511f6cd9a8c0da02f2ec3d819b9
DEEPAK <91515394+kapeed22@users.noreply.github.com>
2026-09-23 15:03:44 +0530

    Update app.ts

--- apps/api/src/app.ts
+++ apps/api/src/app.ts
@@ -2,9 +2,14 @@ import Fastify from "fastify";
 import { Pool } from "pg";
 
 import { healthRoutes } from "./http/routes/health";
+
 import { CategoryRepository } from "./modules/categories/category.repository";
 import { CategoryService } from "./modules/categories/category.service";
 import { categoryRoutes } from "./modules/categories/category.routes";
+
+import { ProductRepository } from "./modules/products/product.repository";
+import { ProductService } from "./modules/products/product.service";
+import { productRoutes } from "./modules/products/product.routes";
 
 export function buildApp(pool: Pool) {
   const app = Fastify({
@@ -14,12 +19,19 @@ export function buildApp(pool: Pool) {
   const categoryRepository = new CategoryRepository(pool);
   const categoryService = new CategoryService(categoryRepository);
 
+  const productRepository = new ProductRepository(pool);
+  const productService = new ProductService(productRepository);
+
   app.register(async (instance) => {
     await healthRoutes(instance, pool);
   });
 
   app.register(async (instance) => {
     await categoryRoutes(instance, categoryService);
+  });
+
+  app.register(async (instance) => {
+    await productRoutes(instance, productService);
   });
 
   return app;
